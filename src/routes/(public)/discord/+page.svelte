1e60193eead40ad2b57cabcc49121c1ecce1751e
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-09-01 20:45:30 -0700

    increase link size

--- src/routes/(public)/discord/+page.svelte
+++ src/routes/(public)/discord/+page.svelte
@@ -6,7 +6,7 @@
 <div class="flex w-full flex-col items-center gap-16 p-4 pb-16">
 	<div class="w-full max-w-4xl">
 		<SectionHeader title="Join our discord server" />
-        <div class="mt-4 mb-4 grid place-items-center">
+        <div class="mt-4 mb-4 grid place-items-center text-xl">
             <ExternalLink
 				href="https://seattleschools-my.sharepoint.com/:w:/g/personal/1rcmclain_seattleschools_org/IQDdTy6CDx8_TatHAPkj-TrbARQ2VadEAwxXiliuI6vu_CI?e=nOO3vJ"
 				text="discord link (access restricted)"
