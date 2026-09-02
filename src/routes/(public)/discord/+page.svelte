ec13ebabbaa45f4d5a6d9192fda5fe91201e309e
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-09-01 20:02:19 -0700

    discord page v2

--- src/routes/(public)/discord/+page.svelte
+++ src/routes/(public)/discord/+page.svelte
@@ -6,12 +6,14 @@
 <div class="flex w-full flex-col items-center gap-16 p-4 pb-16">
 	<div class="w-full max-w-4xl">
 		<SectionHeader title="Join our discord server" />
-		<p class="mt-4 indent-8">
-			The link to join can be found in
-			<ExternalLink
+        <div class="mt-4 mb-4 grid place-items-center">
+            <ExternalLink
 				href="https://seattleschools-my.sharepoint.com/:w:/g/personal/1rcmclain_seattleschools_org/IQDdTy6CDx8_TatHAPkj-TrbARQ2VadEAwxXiliuI6vu_CI?e=nOO3vJ"
-				text="a word document"
-			/> only accessible to sps students and staff. If you are not a student or staff please use the
+				text="discord link (access restricted)"
+			/>
+        </div>
+        <p class="text-center">
+			 the link is only accessible to sps students and staff. If you are not a student or staff please use the
 			<a href="/about#contact" class="underline">contact form</a> making sure to add your email.
 		</p>
 	</div>
